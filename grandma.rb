fe66fa8a400114d75ae95ff130e26079e218af0a
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-06 00:12:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,13 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(input)
+  if input != input.upcase 
+    "HUH?! SPEAK UP, SONNY!"
+  elsif input == input.upcase
+    "NO, NOT SINCE 1938!"
+  elsif input == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
